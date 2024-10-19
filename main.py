45ca701144813457c35d6df1d049a5db289fc282
DeograciasAdanlokonon <adanlokonondeogracias1@gmail.com>
2024-10-19 03:51:08 +0100

    third commit

--- main.py
+++ main.py
@@ -14,36 +14,39 @@ def stockx_search(reference):
 
     url = f"https://stockx.com/search?s={reference}"
 
-    options = Options()
+    options = webdriver.ChromeOptions()
     options.add_argument("--headless")  # Run in headless mode
     options.add_argument("--no-sandbox")  # Required in some environments
     options.add_argument("--disable-dev-shm-usage")  # Avoid /dev/shm issues
     options.add_argument("--disable-gpu")  # Disable GPU (often unnecessary in headless)
     options.add_argument("--disable-extensions")  # Avoid issues with extensions
+    options.add_argument('--disable-software-rasterizer')
 
     driver = webdriver.Chrome(options=options)
-    driver.get(url=url)
 
-    # fetch for the first article
-    article = driver.find_element(By.CSS_SELECTOR, value="#product-results a")
+    try:
+        driver.get(url=url)
 
-    # article name
-    article_name = article.text.split("\n")[0]
-    article_price = article.text.split("\n")[2]
+        # fetch for the first article
+        article = driver.find_element(By.CSS_SELECTOR, value="#product-results a")
 
-    # image link
-    image = article.find_element(By.CSS_SELECTOR, value="img")
-    image_link = image.get_attribute("srcset").split(",")[0]
+        # article name
+        article_name = article.text.split("\n")[0]
+        article_price = article.text.split("\n")[2]
 
-    # final result into a dictionary
-    result = {
-        "name": article_name,
-        "price": int(article_price.replace("$", "")),
-        "image": image_link.split(" ")[0]
-    }
+        # image link
+        image = article.find_element(By.CSS_SELECTOR, value="img")
+        image_link = image.get_attribute("srcset").split(",")[0]
 
-    # close driver
-    driver.quit()
+        # final result into a dictionary
+        result = {
+            "name": article_name,
+            "price": int(article_price.replace("$", "")),
+            "image": image_link.split(" ")[0]
+        }
+    finally:
+        # close driver
+        driver.quit()
 
     # return finale result
     return result
